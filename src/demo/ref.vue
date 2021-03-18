8698fed6ec8afd13805237fd2d6bbcb3aae0d163
Robin-LEI <13661603945@163.com>
2021-03-18 09:05:18 +0800

    add reactive.vue & ref.vue

---
+++ src/demo/ref.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <button @click="click">click add</button>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    let count = ref(0);
+
+    const click = () => {
+      count.value++
+    }
+    return {
+      count,
+      click
+    }
+  },
+}
+</script>
